c2176af62025ae2ad05b5069803062cb8bb83080
OGÜN <samiogunersun@gmail.com>
2022-07-31 11:39:14 +0300

    Proje adın SpringBootMicroServicesJavaBrains olarak değiştirildi.

--- movie-catalog-service/src/main/java/org/ersun/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/org/ersun/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,7 +1,9 @@
 package org.ersun.moviecatalogservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +11,16 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @EnableEurekaClient
 // @EnableEurekaClient anotasyonu Belirli bir versiyona kadar zorunlu fakat belirli bir versiyondan sonra pom.xml e dependenct ekleyip jar ların Classpath te olması yetiyor.
 public class MovieCatalogServiceApplication {
+
+    @Value("${timeout}")
+    private int TIMEOUT;
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCatalogServiceApplication.class, args);
@@ -22,7 +30,7 @@ public class MovieCatalogServiceApplication {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        clientHttpRequestFactory.setConnectTimeout(3000);
+        clientHttpRequestFactory.setConnectTimeout(TIMEOUT);
         return new RestTemplate(clientHttpRequestFactory);
     }
 
